d8c9d6a0bee7b96b71de8be6eedcfbf53bd41e1e
reece-elder <reece.elder@qa.com>
2022-02-21 15:10:30 +0000

    my first print message

---
+++ Beginner/DFE13-intro/src/HelloWorld.java
@@ -0,0 +1,32 @@
+
+public class HelloWorld {
+	
+//	this is a comment with ctrl / 
+//	Comments allow us to write text THAT ISNT READ BY THE COMPUTER
+	
+//	Within Java the ONLY code that runs 
+//	is code read from the main method in this project
+
+//	When writing Python the code runs from the top to the bottom of a file 
+	
+//  Method - Block of code to instruct / tell the computer what to do
+	
+	
+	public static void main(String[] args) {
+		
+		// This is our command to print text
+		// Java is a very picky language
+		// Generally Java commands are lowercase 
+		// All Java commands MUST end in a semicolon
+		System.out.println("Hello all");
+		
+		// To run our project we:
+		// Right click the project, 
+		// Run as Java Application 
+		
+	}
+	
+// Exercise - Create a class that prints out "Hello World"
+//	and "My name is <name>" on two different lines
+
+}
