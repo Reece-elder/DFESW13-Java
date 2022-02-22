c74d32b18f7006b52c072d83dfebdc30facd3d9d
reece-elder <reece.elder@qa.com>
2022-02-22 14:02:02 +0000

    package recap

--- Beginner/DFE-Tuesday/src/com/qa/runner/Runner.java
+++ Beginner/DFE-Tuesday/src/com/qa/runner/Runner.java
@@ -3,16 +3,18 @@ package com.qa.runner;
 // Importing all classes from the package com.qa.colourPrint
 import com.qa.colourPrint.*;
 import com.qa.dataTypes.DataTypes;
+import com.qa.packageRecap.PackageRecap;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		
 		// I want to access the method in class printRed
-		// We want to access the class PrintRed
+		// We want to access the class.method()
 		System.out.println(PrintRed.returnRed());
 		System.out.println(DataTypes.returnBool());
 		System.out.println(DataTypes.returnFalse());
+		System.out.println(PackageRecap.printName());
 	}
 	
 	// Exercise - Convert a recent project to use a runner package as well as a package
