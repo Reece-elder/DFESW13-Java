87793df1ba0e9a6105965368e269a2ed2c1e8b19
reece-elder <reece.elder@qa.com>
2022-02-23 14:09:08 +0000

    dice task

--- Beginner/DFE-Wednesday/src/com/qa/runner/Runner.java
+++ Beginner/DFE-Wednesday/src/com/qa/runner/Runner.java
@@ -1,5 +1,6 @@
 package com.qa.runner;
 
+import com.qa.exercises.DiceTask;
 import com.qa.flowControl.FlowControl;
 import com.qa.flowControl.MethodStack;
 
@@ -7,8 +8,12 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		System.out.println("main added to stack");
-		System.out.println(MethodStack.firstLayer(7));
-		System.out.println(MethodStack.scoreManager(10));
+//		System.out.println(MethodStack.firstLayer(7));
+//		System.out.println(MethodStack.scoreManager(10));
+		
+		System.out.println(DiceTask.exerciseOne());
+		System.out.println(DiceTask.exerciseTwo());
+		
 		System.out.println("main left stack");
 		
 //		System.out.println(FlowControl.printRed());
