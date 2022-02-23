f8197dc4655445627bead57457d4821f58ffec04
reece-elder <reece.elder@qa.com>
2022-02-23 11:05:07 +0000

    calculator exercise

---
+++ Beginner/DFE-Tuesday/src/com/qa/qaRecap/JavaRecap.java
@@ -0,0 +1,17 @@
+package com.qa.qaRecap;
+
+public class JavaRecap {
+	
+	public static void printGreen() {
+		
+		System.out.println("Green");
+		
+	}
+	
+	// Pass in paramaters to our methods / functions so they can use them 
+	// This method takes in a string (text) called name
+	public static void greeting(String name) {
+		System.out.println("Hello " + name);
+	}
+
+}
--- Beginner/DFE-Tuesday/src/com/qa/runner/Runner.java
+++ Beginner/DFE-Tuesday/src/com/qa/runner/Runner.java
@@ -1,27 +1,34 @@
 package com.qa.runner;
 
-// Importing all classes from the package com.qa.colourPrint
-import com.qa.colourPrint.*;
+import com.qa.calculator.CalculatorExercise;
+import com.qa.colourPrint.PrintRed;
 import com.qa.dataTypes.DataTypes;
 import com.qa.operators.Calculator;
 import com.qa.operators.Intro;
 import com.qa.packageRecap.PackageRecap;
+import com.qa.qaRecap.JavaRecap;
+
+
+
+// Importing all classes from the package com.qa.colourPrint
 
 public class Runner {
+
 
 	public static void main(String[] args) {
 		
 		// I want to access the method in class printRed
 		// We want to access the class.method()
-		System.out.println(PrintRed.returnRed());
-		System.out.println(DataTypes.returnBool());
-		System.out.println(DataTypes.returnFalse());
-		System.out.println(PackageRecap.printName());
-		System.out.println(Intro.addNums());
-		System.out.println(Calculator.multiplyzipzoop(12, 5));
-		System.out.println(Calculator.multiplyzipzoop(98, 632));
-		Calculator.helloWorld("Reece", 36);
-		System.out.println(Calculator.isRed("blue"));
+//		System.out.println(PrintRed.returnRed());
+//		System.out.println(DataTypes.returnBool());
+//		System.out.println(DataTypes.returnFalse());
+//		System.out.println(PackageRecap.printName());
+//		System.out.println(Intro.addNums());
+//		System.out.println(Calculator.multiplyzipzoop(12, 5));
+//		System.out.println(Calculator.multiplyzipzoop(98, 632));
+//		Calculator.helloWorld("Reece", 36);
+//		System.out.println(Calculator.isRed("blue"));
+		CalculatorExercise.calculateMethods();
 	}
 	
 	/*
@@ -30,5 +37,12 @@ public class Runner {
 	 		  - Add checks to divide to make sure you don't divide by 0
 	 		  - Add checks to make sure the 2nd number when you subtract is not larger than the 1st
 	 */
+	
+	public static void hmmmmm() {
+		int x = 10;
+		int y = 20;
+		int sum = x + 7;
+		System.out.println(sum);
+	}
 
 }
