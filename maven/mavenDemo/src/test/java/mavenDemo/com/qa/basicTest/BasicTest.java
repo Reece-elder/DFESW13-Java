bb9542d077377b7f13161cd0ee0df3ba0614c4a3
reece-elder <reece.elder@qa.com>
2022-03-03 10:07:30 +0000

    basic testing files

--- maven/mavenDemo/src/test/java/mavenDemo/com/qa/basicTest/BasicTest.java
+++ maven/mavenDemo/src/test/java/mavenDemo/com/qa/basicTest/BasicTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class BasicTest {
 	
+	// Eclipse knows this is a test because of the @Test annotation
 	@Test
 	public void firstTest() {
 		
@@ -14,6 +15,57 @@ public class BasicTest {
 		
 	}
 	
-	// Used maven to install and use an external dependency 
+	// Testing methods generally shouldnt return anything and shouldnt take in params 
+	@Test
+	public void basicDemo() {
+		// We're not testing our code yet
+		// At the minute we're just prototyping tests
+		
+		// I want to test whether 1 + 1 = 2 
+		// You would do the sum yourself, 1 add 1 is equal to 2 
+		
+		// Arrange (Arrange the variables, methods, objects we need)
+		int num1 = 1;
+		int num2 = 1;
+		int sum; 
+		
+		// Act (Doing the thing we are testing)
+		sum = num1 + num2; 
+		
+		// Assert (Validating the thing we're testing gives us the outcome)
+		// Assertions are JUnit built in functions that are used to check values and return true or false
+		Assertions.assertEquals(sum, 2);
+		
+		// assertEquals(2 values)
+		// assertNotEquals(2 values not equal)
+		// assertTrue(boolean query true, sum > 1)
+		// assertFalse(boolean false)
+
+	}
+	
+	@Test
+	public void stringDemo() {
+		// Testing if the string "red" is equal to "red"
+		// Arrange
+		String testQuery = "red";
+		
+		// Act
+		testQuery = "ReD";
+		String newQuery = testQuery.toLowerCase();
+		System.out.println(newQuery);
+		
+		// Assert
+		Assertions.assertEquals("red", newQuery);
+	}
+	
+	// Exercise - Create a test class 
+	// Create tests to test the following: 
+	// 5 * 10 = 50
+	// 1 + 2 + 3 + 4 
+	// 23 / 4 
+	// 27 - 19
+	// "blue" = "blue"
+	// "red" + "triangle" = "red triangle"
+	// 10 * 3 != 40
 
 }
